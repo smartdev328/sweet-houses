76c9db8d8b1afd66734649d15d00e55829c653b5
smartdev328 <solid.dev3028@gmail.com>
2021-11-23 18:25:44 +0200

    update server js to solve server err

--- server.js
+++ server.js
@@ -1,43 +1,23 @@
+const express = require('express');
+const path = require('path');
+const history = require('connect-history-api-fallback');
 
-// const express = require('express');
-// const path = require('path');
-// const history = require('connect-history-api-fallback');
+const app = express();
 
-// const app = express();
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 
-// const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+app.use(staticFileMiddleware);
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}));
+app.use(staticFileMiddleware);
 
-// app.use(staticFileMiddleware);
-// app.use(history({
-//   disableDotRule: true,
-//   verbose: true
-// }));
-// app.use(staticFileMiddleware);
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/index.html'));
+});
 
-// app.get('/', function (req, res) {
-//   res.render(path.join(__dirname + '/dist/index.html'));
-// });
-
-// var server = app.listen(process.env.PORT || 8080, function () {
-//   var port = server.address().port;
-//   console.log("App now running on port", port);
-// });
-
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
-
-const app = express()
-
-//here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
-
-// this * route is to serve project on different page routes except root `/`
-app.get(/.*/, function (req, res) {
-	res.sendFile(path.join(__dirname, '/dist/index.html'))
-})
-
-const port = process.env.PORT || 8080
-app.listen(port)
-console.log(`app is listening on port: ${port}`)
-
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
