f5c86251ad4cc90c209286883cd4bec021e68b91
smartdev328 <solid.dev3028@gmail.com>
2022-02-05 19:18:15 +0200

    Add 2 significant digits for listing prices in the millions

--- src/components/base/ShowMap.vue
+++ src/components/base/ShowMap.vue
@@ -351,14 +351,16 @@ export default {
         return "Sold";
       }
       if (this.type !== "sold") {
-        if (num > 999 && num < 1000000) {
-          return (num / 1000).toFixed(0) + "K"; // convert to K for number from > 1000 < 1 million
-        } else if (num > 1000000) {
-          return (num / 1000000).toFixed(0) + "M"; // convert to M for number from > 1 million
+        if(Math.abs(num) > 999 && Math.abs(num) < 1000000){
+         return Math.sign(num)*((Math.abs(num)/1000).toFixed(2)) + 'k' 
+        }
+        if(Math.abs(num) > 1000000){
+          return Math.sign(num)*((Math.abs(num)/1000000).toFixed(2)) + 'M' 
+        }
         } else if (num < 900) {
           return num; // if value < 1000, nothing to do
         }
-      }
+      
     },
     hideboxselling(){
       this.showbox = false
