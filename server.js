1eff27c2b24f0165732458cd56820d10531e12b8
smartdev328 <solid.dev3028@gmail.com>
2022-03-08 23:41:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get(/.*/, function (req, res) {
 	res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-// Test From Zak  24
+// Test From Zak  25
 
 const port = process.env.PORT || 8080
 app.listen(port)
