3d07801cda303c2677319489fcbde5e997ecff00
smartdev328 <solid.dev3028@gmail.com>
2022-03-12 23:29:03 +0200

    edit img silde

--- src/components/base/homes/HomeDetails.vue
+++ src/components/base/homes/HomeDetails.vue
@@ -145,10 +145,10 @@
                 v-model="slide"
                 :interval="4000"
                 controls
-                
+
                 background="#ababab"
-                img-width="1024"
-                img-height="480"
+                img-width="800"
+                img-height="200"
                 style="text-shadow: 1px 1px 2px #333;"
                 @sliding-start="onSlideStart"
                 @sliding-end="onSlideEnd"
