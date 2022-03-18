d27f1eed0505e79682853a3a4ab33dd5f5c2eb3c
smartdev328 <solid.dev3028@gmail.com>
2022-03-18 05:27:43 -0600

    Update AppRise.vue

--- src/views/AppRise.vue
+++ src/views/AppRise.vue
@@ -2,7 +2,7 @@
     <div>
       <div>
          
-        <iframe src="https://form.jotform.com/220114910502034" style="min-width: 100%; height: 90vh; border: none;"
+        <iframe src="https://form.jotform.com/220114910502034" style="min-width: 100%; height: 75vh; border: none;"
          width="320" height="240" scrolling="yes" frameborder="0" id="JotFormIFrame-220114910502034"
          
           title="Sweetly Engagement and Fee Disclosure" allowfullscreen="true"></iframe>
