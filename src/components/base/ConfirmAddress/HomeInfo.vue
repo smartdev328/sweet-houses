5c0bdb8dec19ad849af18e184653d23e18874bef
smartdev328 <solid.dev3028@gmail.com>
2022-03-21 18:11:43 -0600

    Update HomeInfo.vue

--- src/components/base/ConfirmAddress/HomeInfo.vue
+++ src/components/base/ConfirmAddress/HomeInfo.vue
@@ -581,7 +581,9 @@
                     :options="
                     [
                   { value: 'We want the highest price ...', id: 'We want the highest price with the right improvements' },
-                  { value: 'Two', id: 2 },
+                  { value: 'Sell by a specific date', id: 'Sell by a specific date' },
+                  { value: 'Secure a sale', id: 'Secure a sale to purchase another home' },
+                  { value: 'Been for sale before', id: 'Been for sale before and we don't want that again' },
                 ]"
 
                     :get-option-label="({value}) => `${value}`"
