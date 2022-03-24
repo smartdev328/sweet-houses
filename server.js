c61139928c6d99c9dc240551352bf93a4e93bbbb
smartdev328 <solid.dev3028@gmail.com>
2022-03-24 18:29:00 +0200

    Empty Push

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get(/.*/, function (req, res) {
 	res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-// Test From Zak  34
+// Test From Zak  35
 
 const port = process.env.PORT || 8080
 app.listen(port)
